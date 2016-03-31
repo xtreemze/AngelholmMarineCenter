8bdebfd2f25d88efa73d63fddeedc87422ca76ab
Carlos Velasco <carlos@2activedesign.com>
2016-03-31 11:38:10 +0200

    Design Change, JPGs
    Update design and upload photos. Use of cards and stretch full width.

--- js/init.js
+++ js/init.js
@@ -1,14 +1,17 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-  $('.button-collapse').sideNav({
-      menuWidth: 200, // Default is 240
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-    $('.scrollspy').scrollSpy();
-    $('.slider').slider({full_width: true, indicators: false, height: 600});
+        $('.button-collapse').sideNav({
+            menuWidth: 200, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        });
+        $('.scrollspy').scrollSpy();
+        $('.slider').slider({
+            full_width: true,
+            indicators: true,
+            height: 800
+        });
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
