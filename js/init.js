84f9e63b7ce68e6119c6dd58f608a209de66d08e
Carlos Velasco <carlos@2activedesign.com>
2016-04-01 14:06:16 +0200

    Added Swedish Button

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 (function($) {
     $(function() {
-
+        $(".dropdown-button").dropdown();
         $('.button-collapse').sideNav({
             menuWidth: 200, // Default is 240
             edge: 'left', // Choose the horizontal origin
