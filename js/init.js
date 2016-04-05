9ab74c955ccb4b934462b01ea95e28bb0ac7026b
Carlos Velasco <carlos@2activedesign.com>
2016-04-05 10:52:27 +0200

    Minifying

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@
     $(function() {
         $(".dropdown-button").dropdown();
         $('.button-collapse').sideNav({
-            menuWidth: 200, // Default is 240
+            menuWidth: 320, // Default is 240
             edge: 'left', // Choose the horizontal origin
             closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
         });
