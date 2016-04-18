b8621382a2d64dbc5d8a1b09af4961027e4f8230
Carlos Velasco <carlos@2activedesign.com>
2016-04-18 13:07:03 +0200

    Add sponsor section

--- js/init.js
+++ js/init.js
@@ -1,18 +1,32 @@
-(function($) {
-    $(function() {
-        $(".dropdown-button").dropdown();
-        $('.button-collapse').sideNav({
-            menuWidth: 320, // Default is 240
-            edge: 'left', // Choose the horizontal origin
-            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        });
-        $('.scrollspy').scrollSpy();
-        $('.slider').slider({
-            full_width: true,
-            indicators: true,
-            //height: 500
-        });
-        $('.modal-trigger').leanModal();
-
-    }); // end of document ready
+(function ($) {
+	$(function () {
+		//$(".dropdown-button").dropdown();
+		$('.button-collapse').sideNav({
+			menuWidth: 320, // Default is 240
+			edge: 'left', // Choose the horizontal origin
+			closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+		});
+		$('.scrollspy').scrollSpy();
+		$('.slider').slider({
+			full_width: true,
+			indicators: true,
+			height: 600,
+			transition: 500,
+			interval: 6000
+		});
+		$('.modal-trigger').leanModal();
+		$('.carousel').carousel({
+			time_constant: 200,
+			dist: -50,
+			shift: 40,
+			padding: 50,
+			//full_width: -100
+		});
+		nextCarousel();
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
+var intervalID = window.setInterval(nextCarousel, 3000);
+
+function nextCarousel() {
+	$('.carousel').carousel('next');
+}
