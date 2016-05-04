cd501edf99720809194f6a77fff20b9404ba14c1
Carlos Velasco <carlos@2activedesign.com>
2016-05-04 14:31:41 +0200

    Compliance with W3C standards

--- js/init.js
+++ js/init.js
@@ -1,51 +1,50 @@
-(function ($) {
-	$(function () {
-		$('.button-collapse').sideNav({
+( function ( $ ) {
+	$( function () {
+		$( '.button-collapse' ).sideNav( {
 			menuWidth: 320,
 			// Default is 240
 			edge: 'left',
 			// Choose the horizontal origin
 			closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		});
-		$('.scrollspy').scrollSpy();
-		$('.slider').slider({
+		} );
+		$( '.scrollspy' ).scrollSpy();
+		$( '.slider' ).slider( {
 			full_width: true,
 			indicators: true,
 			height: 600,
 			transition: 500,
 			interval: 6000
-		});
-		$('.modal-trigger').leanModal();
-		$('.carousel').carousel({
+		} );
+		$( '.modal-trigger' ).leanModal();
+		$( '.carousel' ).carousel( {
 			time_constant: 400,
 			dist: -48,
 			shift: 2,
 			padding: 48,
 			//full_width: 100
-		});
+		} );
 
-
-		var options = [{
+		var options = [ {
 			selector: '#sponsors',
 			offset: 500,
 			callback: function () {
-				var intervalID = window.setInterval(nextCarousel, 6000);
+				var intervalID = window.setInterval( nextCarousel, 6000 );
 
 				function nextCarousel() {
-					$('.carousel').carousel('next');
+					$( '.carousel' ).carousel( 'next' );
 				}
 			}
 		}, {
 			selector: '#contact',
 			offset: 220,
 			callback: function () {
-				Materialize.showStaggeredList("#contactList");
+				Materialize.showStaggeredList( "#contactList" );
 			}
-		}];
+		} ];
 
-		Materialize.scrollFire(options);
-
-	});
+		Materialize.scrollFire( options );
+		$( '.hidden' ).hide();
+	} );
 	// end of document ready
-})(jQuery);
+} )( jQuery );
 // end of jQuery name space
