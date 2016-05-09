3c61d7f9dbb26c7613d4c37fca1ed11f42cb39d6
Carlos Velasco <carlos@2activedesign.com>
2016-05-09 14:09:07 +0200

    Apply Masonry Grid

--- js/init.js
+++ js/init.js
@@ -23,7 +23,6 @@
 			padding: 48,
 			//full_width: 100
 		} );
-
 		var options = [ {
 			selector: '#sponsors',
 			offset: 500,
@@ -41,9 +40,13 @@
 				Materialize.showStaggeredList( "#contactList" );
 			}
 		} ];
-
 		Materialize.scrollFire( options );
 		$( '.hidden' ).hide();
+		$( '.grid' ).masonry( {
+			// options
+			itemSelector: '.grid-item',
+			columnWidth: '.grid-item'
+		} );
 	} );
 	// end of document ready
 } )( jQuery );
