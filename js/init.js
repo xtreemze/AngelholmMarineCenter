1528ecf45a6972d774e055fe7463fe89b7e79e4a
Carlos Velasco <carlos@2activedesign.com>
2017-05-31 13:52:35 +0200

    adding cards

--- js/init.js
+++ js/init.js
@@ -1,67 +1,67 @@
 (function jQ($) {
-  $('.button-collapse')
-    .sideNav({
-      menuWidth: 320,
-      // Default is 240
-      edge: 'left',
-      // Choose the horizontal origin
-      closeOnClick: true,
-      // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    });
-  $('.scrollspy')
-    .scrollSpy();
-  $('.slider')
-    .slider({
-      full_width: true,
-      indicators: true,
-      height: 600,
-      transition: 500,
-      interval: 6000,
-    });
-  $('.modal-trigger')
-    .leanModal();
-  $('.carousel')
-    .carousel({
-      time_constant: 400,
-      dist: -48,
-      shift: 2,
-      padding: 48,
-      // full_width: 100
-    });
-  const options = [{
-    selector: '.carousel',
-    offset: 500,
-    callback: function cycle() {
-      function nextCarousel() {
-        $('.carousel')
-          .carousel('next');
-      }
-      window.setInterval(nextCarousel, 5000);
-    },
-  },
-    //  {
-    //   selector: '#contact',
-    //   offset: 220,
-    //   callback: function contacts() {
-    //     Materialize.showStaggeredList('#contactList');
-    //   },
-    // }
-  ];
-  Materialize.scrollFire(options);
-  $('.grid')
-    .masonry({
-      // options
-      // itemSelector: '.grid-item',
-      // columnWidth: '.gridItem',
-      percentPosition: true,
-    });
-  // end of document ready
+    $('.button-collapse')
+        .sideNav({
+            menuWidth: 320,
+            // Default is 240
+            edge: 'left',
+            // Choose the horizontal origin
+            closeOnClick: true,
+            // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        });
+    $('.scrollspy')
+        .scrollSpy();
+    $('.slider')
+        .slider({
+            full_width: true,
+            indicators: true,
+            height: 600,
+            transition: 500,
+            interval: 6000,
+        });
+    $('.modal-trigger')
+        .leanModal();
+    $('.carousel')
+        .carousel({
+            time_constant: 400,
+            dist: -48,
+            shift: 2,
+            padding: 48,
+            // full_width: 100
+        });
+    const options = [{
+            selector: '.carousel',
+            offset: 500,
+            callback: function cycle() {
+                function nextCarousel() {
+                    $('.carousel')
+                        .carousel('next');
+                }
+                window.setInterval(nextCarousel, 5000);
+            },
+        },
+        //  {
+        //   selector: '#contact',
+        //   offset: 220,
+        //   callback: function contacts() {
+        //     Materialize.showStaggeredList('#contactList');
+        //   },
+        // }
+    ];
+    Materialize.scrollFire(options);
+    $('.grid')
+        .masonry({
+            // options
+            // itemSelector: '.grid-item',
+            // columnWidth: '.gridItem',
+            percentPosition: true,
+        });
+    // end of document ready
 }(jQuery));
 // end of jQuery name space
-$(window)
-  .load(function start2() {
-    $('.grid')
-      .masonry({
-        percentPosition: true,
-      });
-  });
+$(document)
+    .ready(function() {
+        $('.grid')
+            .masonry({
+                percentPosition: true,
+            });
+    });
